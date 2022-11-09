17a3cd1e1c63d88ccde9dc377d1eddfb1d25e382
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2022-11-08 20:33:10 -0500

    added js

--- public/index.js
+++ public/index.js
@@ -1,9 +1,21 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+
+   let response= await fetch('http://localhost:9001/counter')
+   //skips promise phase, store value there
+   let obj = await response.json()
+console.log(obj)
+
+   //obj {value: 10}value: 10[[Prototype]]: Object, what is returned
+
+
+
+
+
+    let countValue = obj.value;
 
     function increment(){
         countValue++;
